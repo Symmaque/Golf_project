d5f287ef397d1d132a84a5e43f81cd2bb1f369c4
Aurélien <5aurelientournade5+code@gmail.com>
2022-01-05 17:49:44 +0100

    bugs of generator fixed

--- src/generator/main.rs
+++ src/generator/main.rs
@@ -3,7 +3,7 @@
 use std::fs::File;
 use std::{fs, io};
 
-use std::io::Write;
+use std::io::{stdout, Write};
 use std::env::current_dir;
 use rand::Rng;
 
@@ -12,18 +12,16 @@ extern crate rand;
 
 
 fn main() {
-    let n = get_n();
-
     let path = format!(
         "{}{}{}",
         current_dir().unwrap().to_str().unwrap(), "\\inputs\\", "inputs.txt"
     );
 
+    let n = get_n();
     match fs::remove_file(&path) {
         Ok(_response) => println!("File {:?} already existed so it has been erased before use", &path),
         Err(_error) => ()
     };
-
 
     let mut file: File = fs::OpenOptions::new()
         .write(true)
@@ -34,6 +32,7 @@ fn main() {
 
     write_points(&mut file, &String::from("balls"), n);
     write_points(&mut file, &String::from("holes"), n);
+
 }
 
 fn write_points(file: &mut File, name: &String, n: i32) {
@@ -56,9 +55,17 @@ fn write_points(file: &mut File, name: &String, n: i32) {
 
 
 fn get_n() -> i32 {
-    println!("Please enter n and press Enter");
+    print!("Please enter n and press Enter : ");
+    let _ = stdout().flush();
     let mut a_str = String::new();
     io::stdin().read_line(&mut a_str).expect("read error");
-    a_str.replace("\n", "").parse::<i32>().expect("parse error")
+    a_str = (&a_str).replace("\n", "");
+    a_str = (&a_str).replace("\r", "");
+    let result = a_str.parse::<i32>();
+    let result = match result{
+        Ok(result) => result,
+        Err(error) => {println!("{}", error); 0}
+    };
+    result
 }
 
